2b86eb4c434406447c4a2bf4f8b889856b2115a6
Vivyan Woods <dr.v.woods@gmail.com>
2014-11-30 11:24:15 -0800

    started adding map properties to the js file

--- js/app.js
+++ js/app.js
@@ -11,3 +11,18 @@
 //you should also write the code to filter the set of markers when the user
 //types a search phrase into the search box
 
+$( document ).ready(function() {
+	//create map
+	var map = new google.maps.Map(mapElem, mapOptions);
+});
+
+var mapOptions = {
+	center: {lat: 47.6, lng: -122.3},
+	zoom: 12
+}
+
+//adds map to page in "map" div
+var mapElem = document.getElementById('map');
+
+//creates an InfoWindow
+var infoWin = new google.maps.InfoWindow();
