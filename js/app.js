30db6bd5994627a1996f125a73b66719744f6a3b
Vivyan Woods <dr.v.woods@gmail.com>
2014-12-08 21:03:50 -0800

    got info windows to work now to add searching

--- js/app.js
+++ js/app.js
@@ -12,6 +12,17 @@
 //types a search phrase into the search box
 
 $(document).ready(function() {
+	//adds map to page in "map" div
+	var mapElem = document.getElementById('map');
+
+	//creates an InfoWindow
+	var infoWin = new google.maps.InfoWindow();
+
+	var mapOptions = {
+		center: {lat: 47.6, lng: -122.3},
+		zoom: 12
+	}
+
 	//create map
 	var map = new google.maps.Map(mapElem, mapOptions);
 
@@ -30,63 +41,50 @@ $(document).ready(function() {
 	.always(function() {
 		//called on either success or error cases
 	})
+
+	//for each object in traffic cams, create a marker at location
+	function createMarker(trafficCams, mapObj) {
+		console.log("going to create a marker");
+
+		var position;
+
+		//go through all of the traffic cams
+		$(trafficCams).each(function (i, cam) {
+			//console.log("entered for loop");
+			//console.log(cam);
+
+			var longitude = Number(cam.location.longitude);
+			var latitude = Number(cam.location.latitude);
+			//get new position from camera
+			position = {
+				lat: latitude,
+				lng: longitude
+			};
+			//console.log(cam.location.longitude);
+			//console.log("position:");
+			//console.log(position);
+
+			//creates a new marker
+			var marker = new google.maps.Marker({
+				position: position,
+				map: mapObj
+			});
+
+			//opens new infoWindow when marker is clicked
+			google.maps.event.addListener(marker, 'click', function (evt) {
+				console.log("method called!");
+				console.log(this);
+				//set the info window content
+				infoWin.setContent(
+					cam.cameralabel
+					+ '<br />'
+					+ '<img src="'
+					+ cam.imageurl.url
+					+ '" />'
+				);
+				map.panTo(this.getPosition());
+				infoWin.open(map, this);
+			});
+		}); //end of for each loop
+	}//end of createMarker
 });
-
-var mapOptions = {
-	center: {lat: 47.6, lng: -122.3},
-	zoom: 12
-}
-
-//adds map to page in "map" div
-var mapElem = document.getElementById('map');
-
-//var imgContent;
-
-//creates an InfoWindow
-var infoWin = new google.maps.InfoWindow();
-
-function makeInfoWin() {
-	console.log("method called!");
-	infoWin.open(map, this);
-}
-
-//for each object in traffic cams, create a marker at location
-function createMarker(trafficCams, mapObj) {
-	console.log("going to create a marker");
-
-	//go through all of the traffic cams
-	$(trafficCams).each(function (i, cam) {
-		//console.log("entered for loop");
-		//console.log(cam);
-
-		var longitude = Number(cam.location.longitude);
-		var latitude = Number(cam.location.latitude);
-		//get new position from camera
-		var position = {
-			lat: latitude,
-			lng: longitude
-		};
-		//console.log(cam.location.longitude);
-		//console.log("position:");
-		//console.log(position);
-
-		//creates a new marker
-		var marker = new google.maps.Marker({
-			position: position,
-			map: mapObj
-		});
-
-		//set the info window content
-		infoWin.setContent(
-			cam.cameralabel
-			+ '<br />'
-			+ '<img>'
-			+ cam.imageurl.url
-			+ '</img>'
-		);
-
-		//opens new infoWindow when marker is clicked
-		google.maps.event.addListener(position, 'click', makeInfoWin);
-	});
-}//end of createMarker
-
