a3291802fd0b3d7f317e0a93a72783f82cee0879
Vivyan Woods <dr.v.woods@gmail.com>
2014-12-11 11:51:44 -0800

    cleaned up code and added comments!

--- js/app.js
+++ js/app.js
@@ -3,14 +3,6 @@
 
 "use strict";
 
-//put your code here to create the map, fetch the list of traffic cameras
-//and add them as markers on the map
-//when a user clicks on a marker, you should pan the map so that the marker
-//is at the center, and open an InfoWindow that displays the latest camera
-//image
-//you should also write the code to filter the set of markers when the user
-//types a search phrase into the search box
-
 $(document).ready(function() {
 	//adds map to page in "map" div
 	var mapElem = document.getElementById('map');
@@ -33,7 +25,6 @@ $(document).ready(function() {
 		//success
 		console.log(data);
 		createMarker(data, map);
-		//searchMarkers();
 	})
 	.fail(function(error){
 		//error contains error info
@@ -88,22 +79,28 @@ $(document).ready(function() {
 				map.panTo(this.getPosition());
 				infoWin.open(map, this);
 			});
-			var searchString = ""; //start search string so i can add onto it
+
+			var searchString = ""; //create search string so i can add onto it
 			$('#search').bind('search keyup', function (search) {
-				console.log("search:");
-				console.log(this);
-				console.log("event:");
+				console.log("Key pressed:");
 				console.log(search.keyCode);
 				var searchLength = searchString.length;
+				//this is special code to add (or take away) the 
+				//correct character to my search string
 				if (search.keyCode >= 32 && search.keyCode != 127 && search.keyCode != 173) {
+					//adds basic character from key pressed
 					searchString = searchString + String.fromCharCode(search.keyCode);
 				} else if (search.keyCode == 8 || search.keyCode == 127) {
 					console.log("reached backspace");
+					//recognizes that the delete/backspace key
+					// was pressed and removes last character
 					searchString = searchString.substring(0, searchString.length - 1);
 				} else if (search.keyCode == 13) {
 					console.log("reached enter");
+					//adds on empty string to recognize when enter key has been hit
 					searchString = searchString + "";
 				} else if (search.keyCode == 173) {
+					//special case for when keyCode for key pressed does not match ASCII code
 					searchString = searchString + "-";
 				};
 				searchString = searchString.toLowerCase();
@@ -111,6 +108,7 @@ $(document).ready(function() {
 				searchLength = searchString.length;
 				var camName = cam.cameralabel.toLowerCase();
 				if (searchLength == 0) {
+					//resets the map when there's nothing in search string
 					marker.setMap(map);
 				};
 				if (camName.indexOf(searchString) == -1) {
